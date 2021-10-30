9a6957c7d952804994af6b79a4fcfaa98d543eb2
sarowar hossain <42488988+sarowar75856@users.noreply.github.com>
2021-10-31 03:28:44 +0600

    first Commit

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -67,21 +67,7 @@
           @yield('content')
 
         </div><!-- sl-pagebody -->
-        <footer class="sl-footer">
-            <div class="footer-left">
-                <div class="mg-b-2">Copyright &copy; 2017. Starlight. All Rights Reserved.</div>
-                <div>Made by ThemePixels.</div>
-            </div>
-            <div class="footer-right d-flex align-items-center">
-                <span class="tx-uppercase mg-r-10">Share:</span>
-                <a target="_blank" class="pd-x-5"
-                    href="https://www.facebook.com/sharer/sharer.php?u=http%3A//themepixels.me/starlight"><i
-                        class="fa fa-facebook tx-20"></i></a>
-                <a target="_blank" class="pd-x-5"
-                    href="https://twitter.com/home?status=Starlight,%20your%20best%20choice%20for%20premium%20quality%20admin%20template%20from%20Bootstrap.%20Get%20it%20now%20at%20http%3A//themepixels.me/starlight"><i
-                        class="fa fa-twitter tx-20"></i></a>
-            </div>
-        </footer>
+        @include('admin.body.footer')
     </div><!-- sl-mainpanel -->
     <!-- ########## END: MAIN PANEL ########## -->
 
@@ -102,6 +88,53 @@
     <script src="{{asset('backend/assets/js/starlight.js')}}"></script>
     <script src="{{asset('backend/assets/js/ResizeSensor.js')}}"></script>
     <script src="{{asset('backend/assets/js/dashboard.js')}}"></script>
+
+    
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
+<script src="{{ asset('https://unpkg.com/sweetalert/dist/sweetalert.min.js')}}"></script>
+
+
+<script>
+      @if(Session::has('messege'))
+        var type="{{Session::get('alert-type','info')}}"
+        switch(type){
+            case 'info':
+                 toastr.info("{{ Session::get('messege') }}");
+                 break;
+            case 'success':
+                toastr.success("{{ Session::get('messege') }}");
+                break;
+            case 'warning':
+               toastr.warning("{{ Session::get('messege') }}");
+                break;
+            case 'error':
+                toastr.error("{{ Session::get('messege') }}");
+                break;
+        }
+      @endif
+   </script>  
+
+   <script>  
+       $(document).on("click", "#delete", function(e){
+           e.preventDefault();
+           var link = $(this).attr("href");
+              swal({
+                title: "Are you Want to delete?",
+                text: "Once Delete, This will be Permanently Delete!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+              })
+              .then((willDelete) => {
+                if (willDelete) {
+                     window.location.href = link;
+                } else {
+                  swal("Safe Data!");
+                }
+              });
+          });
+  </script>
 </body>
 
 </html>
